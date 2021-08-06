e39d475bf8456cfb3981b4fe6a45af7990b56e68
EslamHashem <86799810+EslamHashem@users.noreply.github.com>
2021-08-06 13:36:23 +0200

    change

---
+++ App.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "#00ffff"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    const random = getRandom();
+
+    document.body.style.backgroundColor = colors[random];
+    color.textContent = colors[random]
+
+});
+
+function getRandom () {
+    return Math.floor(Math.random() * colors.length);
+}
+
